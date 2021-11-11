dd0b0e86e9509b5bf4b197f0ea567a23f18b78eb
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2021-11-11 15:40:36 +0500

    Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/Node.java
@@ -0,0 +1,35 @@
+package ru.job4j.collection;
+
+public class Node<E> {
+    E item;
+    Node<E> next;
+    Node<E> prev;
+
+    public Node(E item) {
+        this.item = item;
+    }
+
+    public E getItem() {
+        return item;
+    }
+
+    public void setItem(E item) {
+        this.item = item;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<E> prev) {
+        this.prev = prev;
+    }
+}
---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,60 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
+    private int modCount;
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value);
+        if (first == null) {
+            newNode.next = null;
+            newNode.prev = null;
+            first = newNode;
+        } else {
+            last.next = newNode;
+            newNode.prev = last;
+        }
+        last = newNode;
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> rsl = first;
+        for (int i = 0; i < index; i++) {
+            rsl = rsl.next;
+        }
+        return rsl.getItem();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            final int expectedModCount = modCount;
+            Node<E> node = first;
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                } else if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                E rsl = node.getItem();
+                node = node.getNext();
+                return rsl;
+            }
+        };
+    }
+}
