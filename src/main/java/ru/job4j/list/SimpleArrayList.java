a80852dc40c4b3402626e5209f2adb712bb2dc9c
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2021-11-11 13:39:55 +0500

    Динамический список на массиве. [#158 #226916]

--- src/main/java/ru/job4j/list/SimpleArrayList.java
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -11,10 +11,14 @@ public class SimpleArrayList<T> implements List<T> {
         this.container = (T[]) new Object[capacity];
     }
 
+    private T[] arrayIncrease(T[] array) {
+        return Arrays.copyOf(array, array.length * 2);
+    }
+
     @Override
     public void add(T value) {
         if (size == container.length - 1) {
-            container = Arrays.copyOf(container, container.length * 2);
+            container = arrayIncrease(container);
         }
         container[size] = value;
         size++;
@@ -23,16 +27,14 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, container.length);
-        T rsl = container[index];
+        T rsl = get(index);
         container[index] = newValue;
         return rsl;
     }
 
     @Override
     public T remove(int index) {
-        Objects.checkIndex(index, container.length);
-        T rsl = container[index];
+        T rsl = get(index);
         System.arraycopy(container, index + 1,
                 container, index, container.length - index - 1);
         container[container.length - 1] = null;
@@ -43,7 +45,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         return container[index];
     }
 
@@ -60,7 +62,7 @@ public class SimpleArrayList<T> implements List<T> {
 
             @Override
             public boolean hasNext() {
-                return point < size && size != 0;
+                return point < size;
             }
 
             @Override
