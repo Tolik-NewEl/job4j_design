abe8e516b886d4d5ad771f794316191e52f74d65
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2021-11-15 17:40:54 +0500

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -34,6 +34,10 @@ public class ForwardLinked<T> implements Iterable<T> {
         return head.value;
     }
 
+    public boolean isEmpty() {
+        return head == null;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
