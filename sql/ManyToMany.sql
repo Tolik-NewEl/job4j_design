921c6b8bdce48c24e21cba4482113ec8cedd1e16
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2022-04-19 15:23:36 +0500

    0.1. Связи между таблицами [#373615 #290909]

--- sql/ManyToMany.sql
+++ sql/ManyToMany.sql
@@ -8,7 +8,7 @@ create table students(
      name varchar(255)
  );
  
- create table teachers(
+ create table students_teachers(
      id serial primary key,
      student_id int references students(id),
      teacher_id int references teachers(id)
