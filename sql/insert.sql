14fc931504717439de771c73ec1a33350b7a5b1b
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2022-04-27 13:39:13 +0500

    1. Схема прав пользователи и роли. [#1093 #293864]

--- sql/insert.sql
+++ sql/insert.sql
@@ -12,3 +12,14 @@ insert into category(name) values('important');
 insert into state(name) values('new');
 insert into state(name) values('in progress');
 insert into state(name) values('finished');
+
+insert into users(name, role_id) values('Ivan', 1);
+insert into users(name, role_id) values('Petr', 2);
+
+insert into item(name, users_id, category_id, state_id) values('item1', 1, 2, 1);
+insert into item(name, users_id, category_id, state_id) values('item2', 1, 1, 2);
+
+insert into comments(name, item_id) values('emergency!', 1);
+insert into comments(name, item_id) values('need more info', 2);
+
+insert into attachs(name, size, item_id) values('att1', 20, 1);
