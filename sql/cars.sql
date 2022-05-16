214605eac13747f8c2bb43cebbab2093fd26f1c2
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2022-05-16 15:46:56 +0500

    Хранилище машин [#1733 #300508]

--- sql/cars.sql
+++ sql/cars.sql
@@ -43,8 +43,9 @@ insert into carr (name, body_id, engine_id, gearbox_id) values
 	('car7', 2, 2, 5), ('car8', 5, 5, 5),
 	('car9', 7, 7, 7), ('car10', 3, 2, 1);
 
-select c.name as Model, b.name as Type, e.name as Engine, g.name as Gear from carr c
-join body b on c.body_id = b.id join engine e on c.engine_id = e.id join gearbox g on c.gearbox_id = g.id;
+select * from carr c join body b on c.body_id = b.id
+	join engine e on c.engine_id = e.id
+	join gearbox g on c.gearbox_id = g.id;
 
 select b.id, b.name from body b left join carr c on c.body_id = b.id where c.id is null;
 select e.id, e.name from engine e left join carr c on c.engine_id = e.id where c.id is null;
