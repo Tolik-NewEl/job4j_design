7d8c09c933869e8f4200092a3da64906dde3821b
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2022-05-20 13:03:52 +0500

    0.2. PrepareStatement [#379307 #302648]

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -19,10 +19,21 @@ public class ImportDB {
         this.dump = dump;
     }
 
-    public List<User> load() throws IOException {
+    private User check(String line) {
+        String[] tmp = line.split(";");
+        if (tmp.length != 2 || tmp[0].isEmpty() || tmp[1].isEmpty()) {
+            throw new IllegalArgumentException("Input data is wrong!");
+        }
+        User user = new User(tmp[0], tmp[1]);
+        return user;
+    }
+
+    public List<User> load() {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
-            rd.lines().forEach(s -> users.add(new User(s.split(";")[0], s.split(";")[1])));
+            rd.lines().forEach(s -> users.add(check(s)));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return users;
     }
